b17d732bfead08c08c56e2a7cb9e8c1741893b10
root <j.gatete@alustudent.com>
2023-11-12 15:49:35 +0000

    updated

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,36 +1,72 @@
 #!/usr/bin/python3
-"""fetches the title of all hot posts for a given subreddit recursively"""
 
+"""search post function"""
+
+import json
+import operator
 import requests
 
 
-def count_words(subreddit, word_list=[], hot_list=[], after=""):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def count_words(subreddit, word_list, after=None):
+    """get all the keyword count"""
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    PARAMS = {"after": after, "limit": 100}
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, params=PARAMS,
-                                allow_redirects=False)
-        after = RESPONSE.json().get("data").get("after")
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
-        if after is not None:
-            return count_words(subreddit, word_list, hot_list, after)
-
-        new_dict = {}
-        word_list = set([wrd.lower() for wrd in word_list])
-        for title in hot_list:
-            for word in title.split():
-                if word.lower() in new_dict:
-                    new_dict[word.lower()] += 1
-                else:
-                    new_dict.update({word.lower(): 1})
-
-        sorted_dict = sorted(new_dict.items(), key=lambda x: (-x[1], x[0]))
-        for key, value in sorted_dict:
-            if (key in word_list) and (value > 0):
-                print("{}: {}".format(key, value))
-    except Exception:
+    if len(word_list) == 0:
+        print(None)
+        return
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    result = requests.get(url,
+                          headers=headers,
+                          params={"after": after},
+                          allow_redirects=False)
+    if result.status_code != 200:
         return None
+    body = json.loads(result.text)
+    if body["data"]["after"] is not None:
+        newlist = word_list
+        if type(word_list[0]) is str:
+            temp = []
+            for i in word_list:
+                if not any(j['key'].lower() == i.lower() for j in temp):
+                    temp.append({"key": i.lower(), "count": 0, "times": 1})
+                else:
+                    item = list(filter(
+                                lambda search: search['key'] == i.lower(),
+                                temp))
+                    if len(item) > 0:
+                        item[0]["times"] = item[0]["times"] + 1
+            newlist = temp
+        for i in newlist:
+            for j in body["data"]["children"]:
+                for k in j["data"]["title"].lower().split():
+                    if i["key"] == k:
+                        i["count"] = i["count"] + 1
+        return count_words(subreddit, newlist, body["data"]["after"])
+    else:
+        newlist = word_list
+        if type(word_list[0]) is str:
+            temp = []
+            for i in word_list:
+                if not any(j['key'].lower() == i.lower() for j in temp):
+                    temp.append({"key": i.lower(), "count": 0, "times": 1})
+                else:
+                    item = list(filter(
+                                lambda search: search['key'] == i.lower(),
+                                temp))
+                    if len(item) > 0:
+                        item[0]["times"] = item[0]["times"] + 1
+            newlist = temp
+        for i in newlist:
+            for j in body["data"]["children"]:
+                for k in j["data"]["title"].lower().split():
+                    if i["key"] == k:
+                        i["count"] = i["count"] + 1
+        key = operator.itemgetter("key")
+        sorted_list = sorted(word_list, key=key)
+        key = operator.itemgetter("count")
+        sorted_list = sorted(sorted_list, key=key, reverse=True)
+        word_list = sorted_list
+        for i in sorted_list:
+            if i["count"] > 0:
+                print("{}: {}".format(i["key"], i["count"]))
+        return
